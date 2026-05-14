dae193817c230bdb7d8e5fd106b02397260f94e1
zmyapps <zmyapps>
2026-05-14 21:50:00 +0700

    in config add GithubRepo to __init__

--- shared/config/__init__.py
+++ shared/config/__init__.py
@@ -1,4 +1,5 @@
 from .config import config
 from .helpers import get_app_config, get_tweak_config
+from .model import GithubRepo
 
-__all__ = ["config", "get_app_config", "get_tweak_config"]
+__all__ = ["config", "get_app_config", "get_tweak_config", "GithubRepo"]
