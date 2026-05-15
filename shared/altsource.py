c5923938ce39dac2e692175ab14c12c32c43a3eb
zmyapps <zmyapps>
2026-05-15 16:34:52 +0700

    add non-frozen altsource field for script use

--- shared/altsource.py
+++ shared/altsource.py
@@ -1,19 +1,24 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class AltSourceBase(BaseModel):
     model_config = ConfigDict(extra="forbid", frozen=True)
 
 
-class AltSourceApp(AltSourceBase):
-    name: str
-    bundleIdentifier: str
-    version: str
-    localizedDescription: str
-    downloadURL: str
-    iconURL: str
-    versionDate: str
-    size: int
+class AltSourceApp(BaseModel):
+    model_config = ConfigDict(extra="forbid")
+
+    name: str = Field(frozen=True)
+    bundleIdentifier: str = Field(frozen=True)
+    version: str = Field(frozen=True)
+    localizedDescription: str = Field(frozen=True)
+    downloadURL: str = Field(frozen=True)
+    iconURL: str = Field(frozen=True)
+    versionDate: str = Field(frozen=True)
+    size: int = Field(frozen=True)
+
+    # for script usage only
+    asset_id: str | None = Field(default=None, frozen=False)
 
 
 class AltSourceRepo(AltSourceBase):
